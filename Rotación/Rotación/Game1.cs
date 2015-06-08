80f2da163145bdccdb64f60e0e0d8ef0c4fd948f
rurtubiac <randy.urtubia@inacapmail.cl>
2015-06-08 20:46:22 -0300

    Image added.

---
+++ Rotación/Rotación/Game1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Rotación
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Texture2D Textura;
+        Rectangle Rectangulo;
+
+        Vector2 Origen;
+        Vector2 Posicion;
+
+        float rotacion;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            //graphics.IsFullScreen = true;
+            //graphics.PreferredBackBufferWidth = 1280;
+            //graphics.PreferredBackBufferWidth = 720;
+
+
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Textura = Content.Load<Texture2D>("nave");
+            Posicion = new Vector2(250,200);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            Rectangulo = new Rectangle((int)Posicion.X, (int)Posicion.Y, 
+                                        Textura.Width, Textura.Height);
+
+            Origen = new Vector2(Rectangulo.Width/2, Rectangulo.Height/2);
+            // TODO: Add your update logic here
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right)) rotacion += 0.2f;
+            if (Keyboard.GetState().IsKeyDown(Keys.Left)) rotacion -= 0.2f;
+
+            
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(Textura, Posicion, null, 
+                                Color.White, rotacion, Origen, 
+                                1f, SpriteEffects.None, 0);
+            spriteBatch.End();
+            
+
+            base.Draw(gameTime);
+        }
+    }
+}
